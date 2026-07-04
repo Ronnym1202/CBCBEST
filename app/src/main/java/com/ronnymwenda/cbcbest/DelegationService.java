a28332c664a52cc90af315730a367db1cf684d61
Ronny Mwenda <ronnymwenda89@gmail.com>
2026-07-04 18:22:52 +0300

    Add assetlinks.json for TWA verification

---
+++ app/src/main/java/com/ronnymwenda/cbcbest/DelegationService.java
@@ -0,0 +1,14 @@
+package com.ronnymwenda.cbcbest;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
